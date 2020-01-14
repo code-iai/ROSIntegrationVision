3c5a44fa07d5f874a223e5694be29f34ef57f4d4
Tim Fronsee <tfronsee21@gmail.com>
2020-01-14 18:36:06 +0900

    FIX try to fix formatting for github

--- Source/ROSIntegrationVision/ROSIntegrationVision.Build.cs
+++ Source/ROSIntegrationVision/ROSIntegrationVision.Build.cs
@@ -6,56 +6,56 @@ using System.IO;
 
 public class ROSIntegrationVision : ModuleRules
 {
-    private string ModulePath
-    {
-        get { return ModuleDirectory; }
-    }
+  private string ModulePath
+  {
+    get { return ModuleDirectory; }
+  }
 
-    private string ThirdPartyPath
-    {
-        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
-    }
+  private string ThirdPartyPath
+  {
+    get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+  }
 
-    public ROSIntegrationVision (ReadOnlyTargetRules Target) : base(Target)
-  	{
-    		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-            
-        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
-    		
-    		PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
-    		
-    		PublicDependencyModuleNames.AddRange(
-    			new string[]
-    			{
-    				"Core",
-            "Sockets",
-            "Networking",
-            "ROSIntegration"
-    				// ... add other public dependencies that you statically link with here ...
-    			}
-  			);
-    			
-    		PrivateDependencyModuleNames.AddRange(
-    			new string[]
-    			{
-            "Core",
-    				"CoreUObject",
-    				"Engine",
-            "RenderCore",
-    				"Slate",
-    				"SlateCore",
-            "Sockets",
-            "Networking",
-            "ROSIntegration"
-    				// ... add private dependencies that you statically link with here ...	
-    			}
-  			);
-  		
-    		DynamicallyLoadedModuleNames.AddRange(
-    			new string[]
-    			{
-    				// ... add any modules that your module loads dynamically here ...
-    			}
-  			);
-    }
+  public ROSIntegrationVision (ReadOnlyTargetRules Target) : base(Target)
+  {
+    PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+    PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+
+    PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
+
+    PublicDependencyModuleNames.AddRange(
+      new string[]
+      {
+        "Core",
+        "Sockets",
+        "Networking",
+        "ROSIntegration"
+        // ... add other public dependencies that you statically link with here ...
+      }
+    );
+
+    PrivateDependencyModuleNames.AddRange(
+      new string[]
+      {
+        "Core",
+        "CoreUObject",
+        "Engine",
+        "RenderCore",
+        "Slate",
+        "SlateCore",
+        "Sockets",
+        "Networking",
+        "ROSIntegration"
+        // ... add private dependencies that you statically link with here ...	
+      }
+    );
+
+    DynamicallyLoadedModuleNames.AddRange(
+      new string[]
+      {
+        // ... add any modules that your module loads dynamically here ...
+      }
+    );
+  }
 }
