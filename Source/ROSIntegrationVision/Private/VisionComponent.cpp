cd4ced2bdb4d18752305d5f0f5fe3118f09fe8bd
Haowen Shi <harvey.shi@yahoo.com>
2020-07-14 17:23:11 -0400

    Resolve failed compilation of UVisionComponent::convertDepth
    https://github.com/code-iai/ROSIntegrationVision/issues/28

--- Source/ROSIntegrationVision/Private/VisionComponent.cpp
+++ Source/ROSIntegrationVision/Private/VisionComponent.cpp
@@ -746,7 +746,11 @@ void UVisionComponent::convertDepth(const uint16_t *in, __m128 *out) const
 	for (size_t i = 0; i < size; ++i, in += 4, ++out)
 	{
     // Divide by 100 here in order to convert UU (cm) into ROS units (m)
-		*out = _mm_cvtph_ps(_mm_set_epi16(
-        0, 0, 0, 0, *(in + 3), *(in + 2), *(in + 1), *(in + 0))) / 100;
+		*out = _mm_cvtph_ps(
+			_mm_div_epi16(
+				_mm_set_epi16(0, 0, 0, 0, *(in + 3), *(in + 2), *(in + 1), *(in + 0)),
+				_mm_set_epi16(100, 100, 100, 100, 100, 100, 100, 100)
+			)
+		);// / 100;
 	}
 }
